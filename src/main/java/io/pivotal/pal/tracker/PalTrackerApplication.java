9b590a8a247b1fd06c1759124eca8d02f690fd22
Rahul Goyal <rahulsmbj@gmail.com>
2019-07-25 10:53:13 +0530

    solve the travis issue

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,17 +1,20 @@
 package io.pivotal.pal.tracker;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+
+
     @Bean
-    public TimeEntryRepository repo(){
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    public TimeEntryRepository repo(DataSource dataSource){
         return new JdbcTimeEntryRepository(dataSource);
     }
 
